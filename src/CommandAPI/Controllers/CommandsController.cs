49f21ba3e2f02a1e15715255408c32dc4dc4df5e
Rich Bietz <richbietz@antiquariansoftware.com>
2020-07-22 14:48:32 -0700

    Added Entity Framework (EF) migration files to create database in PostgreSQL. Added hooks to 'StartUp' to register database context. Wired database context to command controller.

--- src/CommandAPI/Controllers/CommandsController.cs
+++ src/CommandAPI/Controllers/CommandsController.cs
@@ -1,20 +1,26 @@
 
 using System.Collections.Generic;
+
 using Microsoft.AspNetCore.Mvc;
 
 
 namespace CommandAPI.Controllers
  {
+     using CommandAPI.Models;
 
     [Route("api/[controller]")]
     [ApiController]
     public class CommandsController : ControllerBase
     {
+        private readonly CommandContext _context;
 
+        public CommandsController(CommandContext context) => _context = context;
+
+        // GET:  api/commands
         [HttpGet]
-        public ActionResult<IEnumerable<string>> Get( )
+        public ActionResult<IEnumerable<Command>> GetCommandItems( )
         {
-            return new string[ ] { "this", "is", "hard", "coded" };
+            return _context.CommandItems;
         }
 
     }
